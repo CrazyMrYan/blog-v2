a646cc2bc1322217b3a83931d62aaa7f865f86aa
严家辉 <crazymryan@gmail.com>
2024-03-12 22:40:27 +0800

    docs: update docs

--- .vitepress/theme/components/Page.vue
+++ .vitepress/theme/components/Page.vue
@@ -4,8 +4,12 @@
   <h1 class="blog-title">Blogs</h1>
   <div class="blogList">
     <a class="blog" v-for="item in posts" :href="withBase(item.regularPath)">
-      <div class="title">{{ item.frontMatter.title }}</div>
-      <div class="date">{{ transDate(item.frontMatter.date) }}</div>
+      <!-- {{ posts }} -->
+      <div>
+        <div class="title">{{ item.frontMatter.title }}</div>
+        <div class="date">{{ transDate(item.frontMatter.date) }}</div>
+      </div>
+      <img v-if="item.frontMatter.cover" :src="`/posts/${item.frontMatter.cover}`" alt="" srcset="">
     </a>
   </div>
   <div class="pagination">
@@ -13,11 +17,7 @@
       Previous page
     </button>
     <div v-if="pagesNum > 1">{{ `${pageCurrent}/${pagesNum}` }}</div>
-    <button
-      class="right"
-      v-if="pageCurrent < pagesNum"
-      @click="go(pageCurrent + 1)"
-    >
+    <button class="right" v-if="pageCurrent < pagesNum" @click="go(pageCurrent + 1)">
       Next page
     </button>
   </div>
@@ -139,6 +139,7 @@ const transDate = (date: string) => {
   font-size: 2rem;
   margin-top: 24px;
 }
+
 .blogList {
   padding: 30px 0;
   padding-bottom: 30px;
@@ -147,9 +148,10 @@ const transDate = (date: string) => {
   justify-content: center;
   align-items: center;
 }
+
 .blog {
-  width: 85%;
-  display: block;
+  width: 100%;
+  display: flex;
   border-radius: 10px;
   padding: 0 20px;
   margin: 10px;
@@ -158,20 +160,30 @@ const transDate = (date: string) => {
   box-shadow: 6px 6px var(--vp-c-brand);
   border: 4px solid #3f4e4f;
   cursor: pointer;
+  justify-content: space-between;
+  padding: 6px 20px;
 }
+
+.blog > img {
+  width: 30%;
+}
+
 .blog:hover {
   text-decoration: none;
   transform: translate(-2px, -2px);
   box-shadow: 10px 10px var(--vp-c-brand);
 }
+
 .title {
   color: var(--vp-c-brand-light);
   font-size: 1.2em;
   font-weight: bold;
 }
+
 .date {
   padding-bottom: 7px;
 }
+
 .pagination {
   display: flex;
   align-items: center;
@@ -203,6 +215,7 @@ button::after {
   transform-origin: bottom right;
   transition: transform 0.25s ease-out;
 }
+
 button:hover::after {
   transform: scaleX(1);
   transform-origin: bottom left;
@@ -212,8 +225,8 @@ button:hover::after {
   position: absolute;
   left: 0;
 }
+
 .right {
   position: absolute;
   right: 0;
-}
-</style>
+}</style>
